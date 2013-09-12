ca70d979f1dcbea633307f784e62f08ad0698e2e
Stefan Glase <stefan.glase@googlemail.com>
2013-09-12 23:54:35 +0200

    correct format

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -2,13 +2,13 @@ modules = {
 
     epiceditor {
         defaultBundle false
-        resource url: [dir: '/epiceditor/js/', file: 'epiceditor.min.js']
-        resource url: [dir: '/epiceditor/themes/base/epiceditor.css']
-        resource url: [dir: '/epiceditor/themes/editor/epic-dark.css']
-        resource url: [dir: '/epiceditor/themes/editor/epic-light.css']
-        resource url: [dir: '/epiceditor/themes/preview/bartik.css']
-        resource url: [dir: '/epiceditor/themes/preview/github.css']
-        resource url: [dir: '/epiceditor/themes/preview/preview-dark.css']
+        resource url: [dir: '/epiceditor/js', file: 'epiceditor.min.js']
+        resource url: [dir: '/epiceditor/themes/base', file: 'epiceditor.css']
+        resource url: [dir: '/epiceditor/themes/editor', file: 'epic-dark.css']
+        resource url: [dir: '/epiceditor/themes/editor', file: 'epic-light.css']
+        resource url: [dir: '/epiceditor/themes/preview', file: 'bartik.css']
+        resource url: [dir: '/epiceditor/themes/preview', file: 'github.css']
+        resource url: [dir: '/epiceditor/themes/preview', file: 'preview-dark.css']
     }
 
 }
