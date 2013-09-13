ba248de088bf33116c02068374b3ca037d37480d
Stefan Glase <stefan.glase@googlemail.com>
2013-09-13 09:23:45 +0200

    add contextPath an todo

--- web-app/js/epiceditor-loader.js
+++ web-app/js/epiceditor-loader.js
@@ -1,1 +1,2 @@
-new EpicEditor({ basePath: 'path-to-themes' }).load();
+// TODO set the contextPath so that it is accessible from this piece of code
+new EpicEditor({ basePath: contextPath + '/epiceditor' }).load();
