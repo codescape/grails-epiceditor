8b31fb02e73ec1f039e02ddff45abd349d31a15b
Stefan Glase <stefan.glase@googlemail.com>
2013-09-19 22:02:33 +0200

    document plugin descriptor

--- GrailsEpiceditorGrailsPlugin.groovy
+++ GrailsEpiceditorGrailsPlugin.groovy
@@ -1,55 +1,41 @@
 class GrailsEpiceditorGrailsPlugin {
 
+    // plugin version
     def version = "0.1"
+    
+    // supported grails versions
     def grailsVersion = "2.2 > *"
+    
+    // resources to exclude from packaging
     def pluginExcludes = [
         "grails-app/views/index.gsp",
         "grails-app/views/layouts/main.gsp"
     ]
 
+    // plugin name
     def title = "Grails EpicEditor Plugin"
-    def author = "Stefan Glase"
-    def authorEmail = "stefan.glase@googlemail.com"
     def description = '''Integrates the fabulous EpicEditor with your Grails project'''
 
-    def documentation = "http://grails.org/plugin/grails-epiceditor"
-    def license = "APACHE"
+    // author name and email
+    def author = "Stefan Glase"
+    def authorEmail = "stefan.glase@googlemail.com"
+
+    // further authors involved in the plugin
     def developers = [
             [ name: "Roland Schneider" ],
             [ name: "Torsten Mandry" ]
     ]
+
+    // license
+    def license = "APACHE"
+
+    // url to documentation
+    def documentation = "http://grails.org/plugin/grails-epiceditor"
+
+    // url to issue tracker     
     def issueManagement = [ system: "GitHub", url: "https://github.com/codescape/grails-epiceditor/issues" ]
+    
+    // url to source code
     def scm = [ url: "https://github.com/codescape/grails-epiceditor" ]
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
-    }
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
-    }
 
 }
